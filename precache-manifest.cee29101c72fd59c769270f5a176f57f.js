a8d626050eebb88e963926777722f3479ccbfb6a
msmurali <muralikrishnan0511@gmail.com>
2021-01-26 11:39:34 +0530

    Updates

---
+++ precache-manifest.cee29101c72fd59c769270f5a176f57f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30df998b0408605fa1f06fb852bc120",
+    "url": "/covid-19tracker/index.html"
+  },
+  {
+    "revision": "17df28e95fccad304c2d",
+    "url": "/covid-19tracker/static/css/main.85c9e69f.chunk.css"
+  },
+  {
+    "revision": "05314708e97aeac00448",
+    "url": "/covid-19tracker/static/js/2.cacaf303.chunk.js"
+  },
+  {
+    "revision": "f5846a532341dbf81a6fe52f73022fce",
+    "url": "/covid-19tracker/static/js/2.cacaf303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17df28e95fccad304c2d",
+    "url": "/covid-19tracker/static/js/main.98f17fec.chunk.js"
+  },
+  {
+    "revision": "fe2415666d0e461ffdab",
+    "url": "/covid-19tracker/static/js/runtime-main.061342d7.js"
+  },
+  {
+    "revision": "798983403e6d5d4a1d959716da0435a3",
+    "url": "/covid-19tracker/static/media/covidbg.79898340.png"
+  }
+]);
